aa09a137c69205dca059594f4b439ad79f0acdcb
ElRichi-SA <elrichi.sa@gmail.com>
2026-07-02 13:57:18 -0600

    Creación de DiagnosisService

---
+++ app/services/diagnosis_service.py
@@ -0,0 +1,8 @@
+class DiagnosisService:
+
+    def analyze(self, consultation):
+        """
+        Analiza los parámetros del agua y devuelve
+        una lista de diagnósticos.
+        """
+        return []
